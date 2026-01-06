2fa2a4eca16e05f246c457837581ce109d193f6b
Vladyslav Honchar <dr.tienam123@gmail.com>
2026-01-06 06:02:23 +0200

    refactor(user): add UserRepository implementing interface
    - Add UserRepository with Doctrine implementation
    - Implement UserRepositoryInterface
    - Separate persist from flush (Unit of Work pattern)

---
+++ src/Model/User/Repository/UserRepository.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Model\User\Repository;
+
+use App\Model\User\Contracts\UserRepositoryInterface;
+use App\Model\User\Entity\User\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+
+/**
+ * @extends ServiceEntityRepository<User>
+ */
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface, UserRepositoryInterface
+{
+    public function __construct(
+        ManagerRegistry $registry,
+        private EntityManagerInterface $entityManager,
+    )
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $user::class));
+        }
+
+        $user->setPassword($newHashedPassword);
+        $this->getEntityManager()->persist($user);
+        $this->getEntityManager()->flush();
+    }
+
+    //    /**
+    //     * @return User[] Returns an array of User objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('u')
+    //            ->andWhere('u.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('u.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?User
+    //    {
+    //        return $this->createQueryBuilder('u')
+    //            ->andWhere('u.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+    public function add(User $user): void
+    {
+        $this->entityManager->persist($user);
+    }
+
+    public function save():void
+    {
+        $this->entityManager->flush();
+    }
+}
